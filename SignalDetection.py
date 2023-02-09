7bc0009a54f657642ef72fb3382604f5c58d4759
skandima <114192829+skandima@users.noreply.github.com>
2023-02-08 16:18:06 -0800

    Add files via upload
    Signal Detection Class & Unit Test for Signal Detection

---
+++ SignalDetection.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+import scipy.integrate as spi
+import scipy.stats as stats
+
+class SignalDetection:
+    def __init__(self, hits, misses, falseAlarms, correctRejections):
+        self.hits = hits - 0.0000001
+        self.misses = misses
+        self.falseAlarms = falseAlarms - 0.0000001
+        self.correctRejections = correctRejections
+
+    def hit_rate(self):
+        return self.hits / (self.hits + self.misses)
+
+    def falseAlarm_rate(self):
+        return self.falseAlarms / (self.falseAlarms + self.correctRejections)
+
+    def d_prime(self):
+        hit_rate = self.hit_rate()
+        falseAlarm_rate = self.falseAlarm_rate()
+        z_hit = stats.norm.ppf(hit_rate) 
+        z_falseAlarm = stats.norm.ppf(falseAlarm_rate)
+        return z_hit-z_falseAlarm
+
+    def criterion(self):
+        hit_rate = self.hit_rate()
+        falseAlarm_rate = self.falseAlarm_rate()
+        z_hit = stats.norm.ppf(hit_rate)
+        z_falseAlarm = stats.norm.ppf(falseAlarm_rate)
+        return -0.5*(z_hit + z_falseAlarm)
+
